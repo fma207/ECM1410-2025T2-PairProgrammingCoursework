6e9979a076940d3b5b6f69b256ec3e6493c91a3c
fma207 <fma207@exeter.ac.uk>
2025-03-04 10:36:39 +0000

    Player in progress

---
+++ gamesleague/Player.java
@@ -0,0 +1,28 @@
+private class Player {
+    private String email;
+    public StringdisplayName
+
+
+    private static int idCounter = 0;
+}
+
+//   * Creates a player entry.
+//      *
+//      * @param email The email of the player (unique).
+//      * @param displayName The name of the player.
+//      * @param name The name of the player.
+//      * @param phone The contact phone number of the player or empty string.
+//      * @return The ID of the rider in the system.
+//      * @throws InvalidNameException If the displayName/name is null or starts/ends with whitespace, 
+//      *                              or if the name is less than 5 char / more than 50 char.
+//      *                              or if displayName is less than 1 char/more than 20 char.
+//      * @throws InvalidEmailException If the email is null, empty, or does not contain an '@' character,
+//      * @throws IllegalEmailException if it duplicates an existing email of a player
+//      */
+//     public int createPlayer(String email, String displayName, String name, String phone) 
+//         throws  InvalidEmailException,   
+//                 IllegalEmailException,
+//                 InvalidNameException {
+        
+
+//         return 0; // placeholder so class compiles
